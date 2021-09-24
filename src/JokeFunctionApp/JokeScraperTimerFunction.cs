75dec1e1024af98c3196a4523285622e3f06e20f
Cory Neale <cneale@nowdigital.com.au>
2021-09-24 13:17:35 +1000

    start of job scrape timer.  test message on queue.

---
+++ src/JokeFunctionApp/JokeScraperTimerFunction.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Xml;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Logging;
+using System.Linq;
+
+namespace JokeFunctionApp
+{
+    public static class JokeScraperTimerFunction
+    {
+        [FunctionName("JokeScraperTimerFunction")]
+        public static async Task Run([TimerTrigger("0 */1 * * * *")]TimerInfo myTimer,
+                                    [Queue("joke-queue"), StorageAccount("AzureWebJobsStorage")] ICollector<string> msg, 
+                                    ILogger log)
+        {
+            log.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
+
+            const string JokeSourceRss = "https://www.reddit.com/r/Jokes/top/.rss?t=day";
+
+            var rssString = string.Empty;
+            try
+            {
+                using (var client = new HttpClient())
+                {
+                    using (var response = await client.GetAsync(JokeSourceRss))
+                    {
+                        using (HttpContent content = response.Content)
+                        {
+                            rssString = await content.ReadAsStringAsync();
+                        }
+                    }
+                }
+            }
+            catch(Exception ex)
+            {
+                log.LogError($"Exception fetching url contents [{JokeSourceRss}]");
+                return;
+            }
+
+            XmlDocument xmlDoc = new XmlDocument();
+            xmlDoc.LoadXml(rssString);
+            
+
+            msg.Add("test");
+        }
+    }
+}
